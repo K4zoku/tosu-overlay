0743f34de21a3ba8e840691d68b51faf0adad91b
Kazoku <k4zoku@pm.me>
2025-10-06 14:14:55 +0700

    ci: update gtk3 dependencies

--- .make/project.mk
+++ .make/project.mk
@@ -52,7 +52,7 @@ TEST_BINARY_DIRECTORY:=$(BUILD_DIRECTORY)/$(TEST_DIRECTORY)
 # COMPILER CONFIGURATION #
 # ---------------------- #
 
-LIBRARIES_GTK3:=gtk+-3.0 webkit2gtk-4.0 gtk-layer-shell-0
+LIBRARIES_GTK3:=gtk+-3.0 webkit2gtk-4.1 gtk-layer-shell-0
 LIBRARIES_GTK4:=gtk4 webkitgtk-6.0 gtk4-layer-shell-0
 
 GTK_VERSION?=3
